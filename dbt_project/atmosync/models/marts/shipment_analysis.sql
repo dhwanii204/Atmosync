cd5bb2ebcc2479653c12bb5c9873b35f8a593ee3
Dhwani Shah <dhwanii374@gmail.com>
2026-07-24 20:24:15 +0530

    Initialize dbt analytics project structure

---
+++ dbt_project/atmosync/models/marts/shipment_analysis.sql
@@ -0,0 +1,15 @@
+SELECT
+    t.container_id,
+    t.product,
+    t.temperature,
+    t.moisture,
+    t.air_pressure,
+    t.vibration,
+    t.timestamp,
+    t.distance_remaining_km,
+    t.spoilage_risk,
+    p.price_per_kg,
+    (p.price_per_kg * 1000) AS estimated_container_value
+FROM {{ ref('stg_telemetry') }} t
+LEFT JOIN commodity_prices p
+ON t.product = p.product
---
+++ dbt_project/atmosync/models/staging/stg_telemetry.sql
@@ -0,0 +1,11 @@
+SELECT
+    container_id,
+    product,
+    temperature,
+    moisture,
+    air_pressure,
+    vibration,
+    timestamp,
+    distance_remaining_km,
+    spoilage_risk
+FROM telemetry
